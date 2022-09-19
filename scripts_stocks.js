de86bd4f7fa3075a7725ad4cff082697d32da91b
nattygee <83288489+nattygee@users.noreply.github.com>
2022-09-19 13:34:07 -0400

    learning how to get search results to show in a list

--- scripts_stocks.js
+++ scripts_stocks.js
@@ -6,13 +6,20 @@
 
 const searchField = document.getElementById('stockSearchBar');
 const searchBtn = document.getElementById('submitSearchBtn');
+const resultTemplate = document.getElementById('searchResult');
+const resultsDiv = document.getElementById('testResults');
+
 searchBtn.addEventListener('click', stockSearch);
 
-var phTestText = document.getElementById('phTestText');
+const phTestText = document.getElementById('phTestText');
 
 const phFetch = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=90Y463NPNKFRMA0H';
 
-
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
 
 
 /* autocomplete 👉 searchField.addEventListener('keyup', stockSearch); */
@@ -20,9 +27,12 @@ const phFetch = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&sy
 function stockSearch() {
   
   searchValue = searchField.value;
-  console.log(searchValue);
-
   let tickerSearch = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchValue}&apikey=90Y463NPNKFRMA0H`;
+  removeAllChildNodes(resultsDiv);
+
+  /* let compName = document.getElementById('compName');
+  let compRegion = document.getElementById('compRegion');
+  let compCurrency = document.getElementById('compCurrency'); */
 
   fetch(tickerSearch)
   .then(res => res.json())
@@ -30,7 +40,14 @@ function stockSearch() {
     let matchesLength = searchData['bestMatches'].length;
     console.log(searchData);
     for (let i = 0; i < matchesLength; i++) {
-      phTestText.innerHTML += searchData['bestMatches'][i]['1. symbol'] + ", " + searchData['bestMatches'][i]['2. name'] + ", " + searchData['bestMatches'][i]['4. region'] + ", " + searchData['bestMatches'][i]['8. currency'] + "<br>"; /* 👈 figure out how to make this dynamic*/
+      /* phTestText.innerHTML += searchData['bestMatches'][i]['1. symbol'] + ", " + searchData['bestMatches'][i]['2. name'] + ", " + searchData['bestMatches'][i]['4. region'] + ", " + searchData['bestMatches'][i]['8. currency'] + "<br>"; */ /* 👈 figure out how to make this dynamic*/
+      resultsDiv.appendChild(resultTemplate.content.cloneNode(true));
+      resultTemplate.id = resultTemplate.id.replace(/[0-9]/g, '') + i;
+      console.log(resultTemplate.id);
+      let compTicker = document.getElementById('compTicker');
+      compTicker.id = compTicker.id.replace(/0-9/g, '') + i;
+      compTicker.innerHTML = searchData['bestMatches'][i]['1. symbol'];
+      /* document.getElementById('compTicker').innerHTML = "hey"; */
     };
   })
   
