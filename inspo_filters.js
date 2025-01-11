7b6c634503ee2eb02f88b45af2c1641fc3d10976
nattygee <nattygee@live.com>
2025-01-11 09:30:32 -0500

    add images, try react, include lightbox

---
+++ inspo_filters.js
@@ -0,0 +1,21 @@
+const { Component } = require("react");
+
+/* import React { Component} from "react"; */
+
+import styles from './inspoFilters.module.css';
+
+
+const getMessage = () => "Hello World";
+document.getElementById("inspoFilters").innerHTML = getMessage();
+
+/* const reactDom = require("react-dom")
+
+function InspoFilters() {
+ const {useState} = React
+ const element = <h1>Hello, world!</h1>;   
+ return(
+        element
+    )
+}
+
+ReactDOM.render(<InspoFilters />, document.querySelector("#inspoFilters")) */
