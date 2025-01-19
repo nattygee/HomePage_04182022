ae698eeb70441950e55bc33b3b984271e8f535d0
nattygee <nattygee@live.com>
2025-01-18 19:53:10 -0500

    styled filter buttons but need hz <<< >>> scrollin

--- scripts.js
+++ scripts.js
@@ -435,17 +435,17 @@ function filterInspo(c) {
 mobileMenuBtn.addEventListener('click', openMobileMenu);
 
 function openMobileMenu() {
-    if (mobileMenu.style.opacity === '0') {
+    if (mobileMenu.style.display === 'none') {
         //mobileMenu.style.display = 'flex';
-        mobileMenu.style.opacity = '1';
+        mobileMenu.style.display = 'flex';
         mobileMenuIcon.src = 'images/mobileCloseIcon.svg';
         mobileMenu.style.transition = "200ms";
         mobileMenu.style.transform = "translateY(8px)";
 
-    } else if (mobileMenu.style.opacity === '1')
+    } else if (mobileMenu.style.display === 'flex')
     {
         //mobileMenu.style.display = 'none';
-        mobileMenu.style.opacity = '0';
+        mobileMenu.style.display = 'none';
         mobileMenu.style.transition = '80ms';
         mobileMenu.style.transform = 'translateY(-8px)'
         mobileMenuIcon.src = 'images/mobileMenuIcon.svg';
